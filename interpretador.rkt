709d6d2dd79453c088ecba301d80c5a1f0ec3aec
Alejandro Guerrero Cano <alejogc2002@hotmail.com>
2023-10-16 16:29:02 -0500

    Archivo Inicial

---
+++ interpretador.rkt
@@ -0,0 +1,19 @@
+#lang eopl
+;:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::;
+;; TALLER #3 - FUNDAMENTOS DE COMPILACION E INTERPRETACION DE LP
+
+
+;:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::;
+;; INTEGRANTES 
+; Alejandro Guerrero Cano (2179652)- Alejo101102
+; alejandro.cano@correounivalle.edu.co
+
+; Juan David Loaiza Santiago (2177570)- JuanLoaiza007
+; juan.loaiza.santiago@correounivalle.edu.co
+
+; Juan Sebastian Muñoz Rojas (2177436)- sebastianmr18
+; juan.munoz.rojas@correounivalle.edu.co
+
+
+;::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::;
+;; DESARROLLO
